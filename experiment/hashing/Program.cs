25a8deead03fa1566dcc4cf7f707557272bb9f1f
Scott Beddall <sbeddall@gmail.com>
2022-10-31 22:15:30 -0700

    highlighting the problem!

---
+++ experiment/hashing/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace hashing
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            var shorthashResult = ShortHashGenerator.GenerateShortHash("Azure/azure-sdk-for-pythonsdk/tables/assets.json");
+
+            Console.WriteLine($"Short hash result is \"{shorthashResult}\"");
+
+            return 0;
+        }
+    }
+}
